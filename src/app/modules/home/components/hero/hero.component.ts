28abc10c5276fcbd5ce026232c4b0e99ad8b5295
Joydip <joydip@Joydips-MacBook-Pro.local>
2023-05-29 23:40:37 +0600

    app download fix

--- src/app/modules/home/components/hero/hero.component.ts
+++ src/app/modules/home/components/hero/hero.component.ts
@@ -1,15 +1,29 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
+  bg = 'assets/images/res-1.jpg';
 
-  constructor() { }
+  heroImage = [
+    'assets/images/res-1.jpg',
+    'assets/images/res-bg-1.jpeg',
+    'assets/images/res-bg-2.jpeg',
+    'assets/images/res-2.jpg',
+  ];
+  currentIndex = 0;
+
+  constructor() {}
 
   ngOnInit(): void {
+    // interval(5000).subscribe(() => {
+    //   this.currentIndex = (this.currentIndex + 1) % this.heroImage.length;
+    //   this.bg = this.heroImage[this.currentIndex];
+    // });
   }
-
 }
